4c1471b2fbbdd6ae1255b7f35ac0c9821e26d626
xKaimac <admin@kaimac.com.au>
2024-08-09 21:42:39 +0930

    added auto nav when not logged in

--- frontend/src/components/profile/StatusText.tsx
+++ frontend/src/components/profile/StatusText.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useUser } from "../../utils/UserContext";
-import { Textarea } from "@mantine/core";
+import { Button, Textarea } from "@mantine/core";
 import { FaArrowCircleRight } from "react-icons/fa";
 import { useMutation } from "@tanstack/react-query";
+import { MdEdit } from "react-icons/md";
 
 interface UploadData {
   userStatus: string;
@@ -10,15 +12,13 @@ interface UploadData {
 
 const StatusText = () => {
   const { userData, updateUserData } = useUser();
-
   const userId = userData.user.id;
-  let userStatus = userData.user.status_text;
+  const [userStatus, setUserStatus] = useState(userData.user.status_text);
 
   const updateStatusText = async ({ userStatus, userId }: UploadData) => {
     const formData = new FormData();
     formData.append("userStatus", userStatus);
     formData.append("userId", userId);
-
     const response = await fetch(
       `${import.meta.env.VITE_BACKEND_URL}/user/update-status-text`,
       {
@@ -37,15 +37,17 @@ const StatusText = () => {
     mutationFn: updateStatusText,
     onSuccess: (data) => {
       updateUserData({ status_text: data.result.status_text });
+      setUserStatus(data.result.status_text);
     },
   });
 
   const handleClick = () => {
+    toggleTextArea();
     mutation.mutate({ userStatus, userId: userId });
   };
 
   const handleOnChange = (event: any) => {
-    userStatus = event.target.value;
+    setUserStatus(event.target.value);
   };
 
   const EnterComponent = () => {
@@ -56,19 +58,43 @@ const StatusText = () => {
     );
   };
 
+  const toggleTextArea = () => {
+    const textArea = document.getElementById("textArea");
+    const statusText = document.getElementById("statusText");
+
+    textArea!.style.display =
+      textArea!.style.display === "block" ? "none" : "block";
+    statusText!.style.display =
+      statusText!.style.display === "none" ? "block" : "none";
+  };
+
   return (
-    <div className="w-3/4 lg:w-1/2 xl:w-1/3 pt-5">
-      <Textarea
-        radius="xl"
-        maxLength={255}
-        autosize
-        minRows={1}
-        maxRows={8}
-        defaultValue={userData.user.status_text}
-        rightSection={<EnterComponent />}
-        onChange={handleOnChange}
-      />
-    </div>
+    <>
+      <div
+        id="statusText"
+        className="flex flex-row w-3/4 lg:w-1/2 xl:w-1/3 pt-5 text-center"
+      >
+        <p>{userStatus}</p>
+        <Button variant="transparent" onClick={toggleTextArea}>
+          <MdEdit />
+        </Button>
+      </div>
+      <div
+        id="textArea"
+        className="display-none w-3/4 lg:w-1/2 xl:w-1/3 pt-5 hidden"
+      >
+        <Textarea
+          radius="xl"
+          maxLength={255}
+          autosize
+          minRows={1}
+          maxRows={8}
+          value={userStatus}
+          rightSection={<EnterComponent />}
+          onChange={handleOnChange}
+        />
+      </div>
+    </>
   );
 };
 
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,22 @@
-import { Outlet, createRootRoute, useLocation } from "@tanstack/react-router";
+import {
+  Navigate,
+  Outlet,
+  createRootRoute,
+  useLocation,
+} from "@tanstack/react-router";
 import AppLayout from "../components/layouts/AppLayout";
+import { useUser } from "../utils/UserContext";
 
 export const Route = createRootRoute({
   component: () => {
     const location = useLocation();
     const isLoginPage = location.pathname.toLowerCase() === "/login";
     const isUsernamePage = location.pathname.toLowerCase() === "/username";
+    const { userData } = useUser();
+
+    if (!userData && !isLoginPage) {
+      return <Navigate to="/login" />;
+    }
 
     if (isLoginPage || isUsernamePage) {
       return <Outlet />;
