4ad8fb58bb126ae9107de91fe33d9765c7b4700b
xKaimac <admin@kaimac.com.au>
2024-09-03 11:53:36 +0930

    removed npm i from start and added output dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import viteReact from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), viteReact()],
+  build: {
+    outDir: "dist"
+  }
 });
